d77de032bc4a3acc47c173c34f6de83477b47756
luochenhao <3453851623@qq.com>
2023-06-13 14:53:25 +0800

    comment(httpClient&httpParseWrapper): add serveral comments.

--- server/src/base/version.h
+++ server/src/base/version.h
@@ -1,6 +1,15 @@
+/*
+ Reviser: Polaris_hzn8
+ Email: 3453851623@qq.com
+ filename: version.h
+ Update Time: Tue 13 Jun 2023 12:49:32 CST
+ brief: 
+*/
+
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
 #define VERSION "1.0"
 
 #endif
+
