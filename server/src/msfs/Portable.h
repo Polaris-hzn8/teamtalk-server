1059a1a5b52acc72786259f9ad5ac0213eaaf406
luochenhao <3453851623@qq.com>
2023-06-15 00:53:14 +0800

    login_server&msfs

--- server/src/msfs/Portable.h
+++ server/src/msfs/Portable.h
@@ -1,25 +1,27 @@
 /*
-* 数据类型与一些可移植相关定义
-*
+ Reviser: Polaris_hzn8
+ Email: 3453851623@qq.com
+ filename: Portable.h
+ Update Time: Thu 15 Jun 2023 00:52:19 CST
+ brief: 数据类型与一些可移植相关定义
 */
 
 #ifndef _PORTABLE_H_
 #define _PORTABLE_H_
 
-typedef signed char 		s8;
-typedef unsigned char 		u8;
-typedef signed short 		s16;
-typedef unsigned short 		u16;
-typedef signed int 		s32;
-typedef unsigned int 		u32;
-typedef long long 		s64;
-typedef unsigned long long 	u64;
-typedef unsigned int 		uint;
-typedef unsigned char 		byte;
+typedef signed char s8;
+typedef unsigned char u8;
+typedef signed short s16;
+typedef unsigned short u16;
+typedef signed int s32;
+typedef unsigned int u32;
+typedef long long s64;
+typedef unsigned long long u64;
+typedef unsigned int uint;
+typedef unsigned char byte;
 
 #ifndef LITTLE_ENDIAN
 #define LITTLE_ENDIAN
 #endif
-
 
 #endif
--- server/src/msfs/StringUtils.h
+++ server/src/msfs/StringUtils.h
@@ -1,22 +1,30 @@
+/*
+ Reviser: Polaris_hzn8
+ Email: 3453851623@qq.com
+ filename: StringUtils.h
+ Update Time: Thu 15 Jun 2023 00:52:42 CST
+ brief:
+*/
+
 #include <string>
 
 namespace msfs {
-	// Replaces all occurrences of "search" with "replace".
-	void replace_substrs(const char *search,
-						 size_t search_len,
-						 const char *replace,
-						 size_t replace_len,
-						 std::string *s);
-	
-	// True iff s1 starts with s2.
-	bool starts_with(const char *s1, const char *s2);
-	
-	// True iff s1 ends with s2.
-	bool ends_with(const char *s1, const char *s2);
-	
-	// Remove leading and trailing whitespaces.
-	std::string string_trim(const std::string& s);
-	
-	std::string string_prefix(const std::string& s, const char *sep = ".");
-	std::string string_suffix(const std::string& s, const char *sep = ".");
+// Replaces all occurrences of "search" with "replace".
+void replace_substrs(const char* search,
+    size_t search_len,
+    const char* replace,
+    size_t replace_len,
+    std::string* s);
+
+// True iff s1 starts with s2.
+bool starts_with(const char* s1, const char* s2);
+
+// True iff s1 ends with s2.
+bool ends_with(const char* s1, const char* s2);
+
+// Remove leading and trailing whitespaces.
+std::string string_trim(const std::string& s);
+
+std::string string_prefix(const std::string& s, const char* sep = ".");
+std::string string_suffix(const std::string& s, const char* sep = ".");
 }
